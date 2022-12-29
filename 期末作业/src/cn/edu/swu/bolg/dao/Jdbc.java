3abe9eb0b2031867eca16d2a3b43b23c0ff7bef4
LLiyilin <2360127269@qq.com>
2022-12-29 16:33:41 +0800

    期末作业添加war包jar包

--- 期末作业/src/cn/edu/swu/bolg/dao/Jdbc.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.swu.bolg.dao;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface Jdbc<T> {
-    public T visit(ResultSet rs) throws SQLException;
-}
--- 期末作业/src/cn/edu/swu/bolg/service/BlogService.java
+++
@@ -1,4 +0,0 @@
-package cn.edu.swu.bolg.service;
-
-public class BlogService {
-}
